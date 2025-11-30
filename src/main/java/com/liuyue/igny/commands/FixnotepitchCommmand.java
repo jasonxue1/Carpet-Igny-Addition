d1f132b1d02fb1b790a08c752d7eb506eef42119
liuyuexiaoyu1 <liuyuexiaoyu1@gmail.com>
2025-11-30 19:39:19 +0800

    添加了指令/fixnotepitch的可选参数：pitch

--- src/main/java/com/liuyue/igny/commands/FixnotepitchCommmand.java
+++ src/main/java/com/liuyue/igny/commands/FixnotepitchCommmand.java
@@ -3,6 +3,7 @@ package com.liuyue.igny.commands;
 import com.liuyue.igny.IGNYSettings;
 import com.liuyue.igny.utils.PlayerCommandPermissions;
 import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.arguments.IntegerArgumentType;
 import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import net.minecraft.commands.CommandSourceStack;
@@ -22,15 +23,16 @@ public class FixnotepitchCommmand {
                 Commands.literal("fixnotepitch")
                         .requires(source -> PlayerCommandPermissions.canUseCommand(source, IGNYSettings.CommandFixnotepitch))
                         .then(Commands.argument("pos1", BlockPosArgument.blockPos())
-                        .then(Commands.argument("pos2", BlockPosArgument.blockPos())
-                        .executes(context -> executeCommand(context))
-        )));
+                                .then(Commands.argument("pos2", BlockPosArgument.blockPos())
+                                        .executes(context -> executeCommand(context, 0)) // 默认音高为0
+                                        .then(Commands.argument("pitch", IntegerArgumentType.integer(0, 24))
+                                                .executes(context -> executeCommand(context, IntegerArgumentType.getInteger(context, "pitch"))))
+                                )));
     }
 
-    private static int executeCommand(CommandContext<CommandSourceStack> context) throws CommandSyntaxException {
+    private static int executeCommand(CommandContext<CommandSourceStack> context, int targetPitch) throws CommandSyntaxException {
         final BlockPos pos1 = BlockPosArgument.getLoadedBlockPos(context, "pos1");
         final BlockPos pos2 = BlockPosArgument.getLoadedBlockPos(context, "pos2");
-
         final CommandSourceStack source = context.getSource();
         final Level level = source.getLevel();
 
@@ -41,35 +43,41 @@ public class FixnotepitchCommmand {
         final int maxY = Math.max(pos1.getY(), pos2.getY());
         final int maxZ = Math.max(pos1.getZ(), pos2.getZ());
 
-        final int[] changedCount = {0};
+        final int changedCount = calculateNoteBlockChanges(level, minX, minY, minZ, maxX, maxY, maxZ, targetPitch);
 
-        source.sendSuccess(() -> {
-            int count = 0;
-            for (int x = minX; x <= maxX; x++) {
-                for (int y = minY; y <= maxY; y++) {
-                    for (int z = minZ; z <= maxZ; z++) {
-                        BlockPos pos = new BlockPos(x, y, z);
-                        if (level.isLoaded(pos)) {
-                            BlockState state = level.getBlockState(pos);
-                            if (state.is(Blocks.NOTE_BLOCK) && state.hasProperty(BlockStateProperties.NOTE)) {
-                                int currentNote = state.getValue(BlockStateProperties.NOTE);
-                                if (currentNote != 0) {
-                                    BlockState newState = state.setValue(BlockStateProperties.NOTE, 0);
-                                    if (IGNYSettings.FixnotepitchUpdateBlock) {
-                                        level.setBlock(pos, newState, 3);
-                                    }else {
-                                        level.setBlock(pos, newState, 2 | 16);
-                                    }
-                                    count++;
+        source.sendSuccess(() ->
+                        Component.translatable("igny.command.fixnotepitch.success", changedCount, targetPitch),
+                true
+        );
+
+        return changedCount;
+    }
+
+    private static int calculateNoteBlockChanges(Level level, int minX, int minY, int minZ, int maxX, int maxY, int maxZ, int targetPitch) {
+        int changedCount = 0;
+
+        for (int x = minX; x <= maxX; x++) {
+            for (int y = minY; y <= maxY; y++) {
+                for (int z = minZ; z <= maxZ; z++) {
+                    BlockPos pos = new BlockPos(x, y, z);
+                    if (level.isLoaded(pos)) {
+                        BlockState state = level.getBlockState(pos);
+                        if (state.is(Blocks.NOTE_BLOCK) && state.hasProperty(BlockStateProperties.NOTE)) {
+                            int currentNote = state.getValue(BlockStateProperties.NOTE);
+                            if (currentNote != targetPitch) {
+                                BlockState newState = state.setValue(BlockStateProperties.NOTE, targetPitch);
+                                if (IGNYSettings.FixnotepitchUpdateBlock) {
+                                    level.setBlock(pos, newState, 3);
+                                } else {
+                                    level.setBlock(pos, newState, 2 | 16);
                                 }
+                                changedCount++;
                             }
                         }
                     }
                 }
             }
-            return Component.translatable("igny.command.fixnotepitch.success", count);
-        }, true);
-
-        return changedCount[0];
+        }
+        return changedCount;
     }
 }
