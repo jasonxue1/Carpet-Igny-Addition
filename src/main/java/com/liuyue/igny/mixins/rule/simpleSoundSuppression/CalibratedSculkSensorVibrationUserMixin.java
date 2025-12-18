6e798abe4cf3ab3fd6a6ca1c3f9a86a717ff5682
liuyuexiaoyu1 <liuyuexiaoyu1@gmail.com>
2025-12-18 20:29:15 +0800

    调整字段名 调整安全声音抑制器逻辑

--- src/main/java/com/liuyue/igny/mixins/rule/simpleSoundSuppression/CalibratedSculkSensorVibrationUserMixin.java
+++ src/main/java/com/liuyue/igny/mixins/rule/simpleSoundSuppression/CalibratedSculkSensorVibrationUserMixin.java
@@ -18,17 +18,17 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(targets = "net/minecraft/world/level/block/entity/CalibratedSculkSensorBlockEntity$VibrationUser")
 public class CalibratedSculkSensorVibrationUserMixin {
     @Unique
-    private CalibratedSculkSensorBlockEntity igny$capturedSensor;
+    private CalibratedSculkSensorBlockEntity igny$blockEntity;
 
     @Inject(method = "<init>", at = @At("RETURN"))
-    private void igny$onInit(CalibratedSculkSensorBlockEntity calibratedSculkSensorBlockEntity, BlockPos blockPos, CallbackInfo ci) {
-        this.igny$capturedSensor = calibratedSculkSensorBlockEntity;
+    private void createBlockEntity(CalibratedSculkSensorBlockEntity calibratedSculkSensorBlockEntity, BlockPos blockPos, CallbackInfo ci) {
+        this.igny$blockEntity = calibratedSculkSensorBlockEntity;
     }
 
     @Inject(method = "canReceiveVibration", at = @At("RETURN"))
     private void canReceiveVibration(ServerLevel serverLevel, BlockPos blockPos, Holder<GameEvent> holder, GameEvent.Context context, CallbackInfoReturnable<Boolean> cir) {
-        if (this.igny$capturedSensor == null) return;
-        Component component = this.igny$capturedSensor.components().get(DataComponents.CUSTOM_NAME);
+        if (this.igny$blockEntity == null) return;
+        Component component = this.igny$blockEntity.components().get(DataComponents.CUSTOM_NAME);
         if (component != null) {
             if (RuleUtils.canSoundSuppression(component.getString()) && cir.getReturnValueZ()) {
                 throw new IllegalArgumentException("[Carpet-Igny-Addition] Simple SoundSuppression");
