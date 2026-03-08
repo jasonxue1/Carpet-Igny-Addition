942feb4f40e006c9520d3b820716d29d57c1bf4e
liuyuexiaoyu1 <liuyuexiaoyu1@gmail.com>
2026-03-08 22:55:42 +0800

    🐛修复：编译失败

--- src/main/java/com/liuyue/igny/mixins/rule/podzolSpread/BlockMixin.java
+++ src/main/java/com/liuyue/igny/mixins/rule/podzolSpread/BlockMixin.java
@@ -11,17 +11,9 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(BlockBehaviour.class)
 public abstract class BlockMixin {
-    @Inject(method = "isRandomlyTicking", at = @At("HEAD"), cancellable = true)
-    private void enableTicking(BlockState blockState, CallbackInfoReturnable<Boolean> cir) {
-        if (blockState.is(Blocks.PODZOL)) {
-            cir.setReturnValue(true);
-        }
-    }
-
     @Inject(method = "randomTick", at = @At("HEAD"))
     private void randomTick(BlockState blockState, ServerLevel serverLevel, BlockPos blockPos, RandomSource randomSource, CallbackInfo ci) {
         if (IGNYSettings.podzolSpread && blockState.is(Blocks.PODZOL)) {
