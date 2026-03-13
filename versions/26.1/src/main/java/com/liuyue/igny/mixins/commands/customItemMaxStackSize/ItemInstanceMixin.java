992bd29590a1bd385b508abe7cf20287068c1d4c
liuyuexiaoyu1 <liuyuexiaoyu1@gmail.com>
2026-03-13 23:14:28 +0800

    🐛修复：编译失败

--- versions/26.1/src/main/java/com/liuyue/igny/mixins/commands/customItemMaxStackSize/ItemInstanceMixin.java
+++ versions/26.1/src/main/java/com/liuyue/igny/mixins/commands/customItemMaxStackSize/ItemInstanceMixin.java
@@ -2,13 +2,10 @@ package com.liuyue.igny.mixins.commands.customItemMaxStackSize;
 
 import com.liuyue.igny.IGNYSettings;
 import com.liuyue.igny.data.CustomItemMaxStackSizeDataManager;
-import com.liuyue.igny.utils.InventoryUtils;
-import com.liuyue.igny.utils.RuleUtils;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemInstance;
 import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
