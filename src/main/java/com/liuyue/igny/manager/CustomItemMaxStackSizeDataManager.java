03ec0ef8d5379adb48ec8a9b1c9282307f2826b3
liuyuexiaoyu1 <liuyuexiaoyu1@gmail.com>
2026-03-21 17:23:33 +0800

    🐛修复：编译失败

--- src/main/java/com/liuyue/igny/manager/CustomItemMaxStackSizeDataManager.java
+++ src/main/java/com/liuyue/igny/manager/CustomItemMaxStackSizeDataManager.java
@@ -99,7 +99,12 @@ public class CustomItemMaxStackSizeDataManager extends BaseDataManager<Map<Strin
             } catch (Exception e) {
                 ResourceLocation rl = ResourceLocation.tryParse(pattern);
                 if (rl != null && BuiltInRegistries.ITEM.containsKey(rl)) {
-                    Item item = BuiltInRegistries.ITEM.get(rl);
+                    Item item = BuiltInRegistries.ITEM.
+                            //#if MC >= 12102
+                            //$$ getValue(rl);
+                            //#else
+                                    get(rl);
+                    //#endif
                     newRules.add(new StackRule(pattern, stack -> stack.is(item), count));
                 }
             }
