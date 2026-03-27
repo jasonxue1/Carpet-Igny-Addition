d08f4319afb679f3edabf494ff88c2c4f077b625
liuyuexiaoyu1 <liuyuexiaoyu1@gmail.com>
2026-03-27 21:11:35 +0800

    🐛修复：编译失败

--- src/main/java/com/liuyue/igny/mixins/carpet/fix/fakePlayerMemoryLeakFix/NetHandlerPlayServerFakeMixin.java
+++ src/main/java/com/liuyue/igny/mixins/carpet/fix/fakePlayerMemoryLeakFix/NetHandlerPlayServerFakeMixin.java
@@ -3,21 +3,21 @@ package com.liuyue.igny.mixins.carpet.fix.fakePlayerMemoryLeakFix;
 //#if MC < 26.1
 import carpet.patches.NetHandlerPlayServerFake;
 import com.liuyue.igny.IGNYSettings;
-import net.minecraft.network.Connection;
 import net.minecraft.network.protocol.Packet;
-import net.minecraft.server.MinecraftServer;
-import net.minecraft.server.level.ServerPlayer;
-//#if MC >= 12002
-import net.minecraft.server.network.CommonListenerCookie;
-//#endif
-import net.minecraft.server.network.ServerGamePacketListenerImpl;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 //#else
 //$$ import com.liuyue.igny.utils.compat.DummyClass;
 //#endif
+import net.minecraft.network.Connection;
 import org.spongepowered.asm.mixin.Mixin;
+import net.minecraft.server.network.ServerGamePacketListenerImpl;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.level.ServerPlayer;
+//#if MC >= 12002
+import net.minecraft.server.network.CommonListenerCookie;
+//#endif
 
 //#if MC >= 26.1
 //$$ @Mixin(DummyClass.class)
